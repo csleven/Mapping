70a7dc7354d09df373093cf7671ad6fe57f9064e
csleven <catriona.leven@alumni.ubc.ca>
2016-05-19 15:45:10 -0700

    I made a map!

--- Mapping Code.R
+++ Mapping Code.R
@@ -10,12 +10,12 @@ library(maptools)
 install.packages("maptools")
 
 ##Make a Simple Map
-canada.cities
+canada.cities 
 map(database="world", region = "Canada", xlim=c(-141,-53), 
     ylim=c(40,85), fill = TRUE, col="gray90")
 ##Note: xlim = latitude, ylim = longitude
 
-##Plotting Points on Map
-#Open file with lat long data
+##Plotting Points on Mapn
+#Open file with lat long data xs
 species <-read.csv("Cornus canadensis.csv", stringsAsFactors = FALSE, strip.white = TRUE, 
                    na.strings = c("NA",""),h = T, as.is = T)
