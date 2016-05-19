e9cc25926980ed51468ea8fa0ab23321de329146
csleven <catriona.leven@alumni.ubc.ca>
2016-05-19 15:32:24 -0700

    Just starting out...

---
+++ Mapping Code.R
@@ -0,0 +1,21 @@
+##Using the Mapping Tutorial by Kimberly Gilbert
+
+## Set Working Directory
+setwd("~/Desktop/Work with Amy/Mapping")
+
+##Open Libraries
+library(maps)
+library(mapdata)
+library(maptools)
+install.packages("maptools")
+
+##Make a Simple Map
+canada.cities
+map(database="world", region = "Canada", xlim=c(-141,-53), 
+    ylim=c(40,85), fill = TRUE, col="gray90")
+##Note: xlim = latitude, ylim = longitude
+
+##Plotting Points on Map
+#Open file with lat long data
+species <-read.csv("Cornus canadensis.csv", stringsAsFactors = FALSE, strip.white = TRUE, 
+                   na.strings = c("NA",""),h = T, as.is = T)
